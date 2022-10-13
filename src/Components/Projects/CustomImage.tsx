3f3290e8337bd85b73be57620fc42b8a2f0417ae
Matias Netto <matiasnetto03@gmail.com>
2022-10-13 16:07:24 -0300

    optimized images

--- src/Components/Projects/CustomImage.tsx
+++ src/Components/Projects/CustomImage.tsx
@@ -1,15 +1,20 @@
+import { desktopQuery } from 'Common/styles/breakpoints';
 import colors from 'Common/styles/colors';
 import Image from 'next/image';
 import { FC, HTMLAttributes, HTMLProps } from 'react';
 import styled from 'styled-components';
 
 const ImageComponent = styled(Image)`
-  padding: 1rem !important;
+  padding: 0.5rem !important;
   background: ${colors.lightBackground};
+
+  ${desktopQuery} {
+    padding: 1rem !important;
+  }
 `;
 
 const CustomImage: FC<HTMLProps<HTMLImageElement>> = ({ src, alt }) => {
-  return <ImageComponent src={src ?? ''} alt={alt} height={590} width={960} objectFit="scale-down" />;
+  return <ImageComponent src={src ?? ''} alt={alt} height={560} width={960} objectFit="contain" />;
 };
 
 export default CustomImage;
